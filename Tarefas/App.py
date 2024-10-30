e3a0a262e0a94a713ecfcb3e8b043d55d97ad1c1
Javelson Canzenze <javelsoncanzenze7@gmail.com>
2024-10-30 15:01:02 +0100

    finalizando a lista

--- Tarefas/App.py
+++ Tarefas/App.py
@@ -1,16 +1,25 @@
 from flask import Flask, render_template, request, redirect, url_for
 
-#Inicializar o Flask
+# Inicializar o Flask
 app = Flask(__name__)
 
-#Criar rota
+# Lista para armazenar as tarefas
+tarefas = []
 
 @app.route('/')
 def index():
-    return render_template('Index.html')
+    return render_template('Index.html', tarefas=tarefas)
 
-@app.route('/Login')
-def Login():
-    return render_template('Login.html')
+@app.route("/add", methods=['POST'])
+def add():
+    tarefa = request.form['tarefa']
+    tarefas.append(tarefa)
+    return redirect(url_for('index'))
 
-app.run(debug=True)
+@app.route("/delete/<int:index>", methods=['GET'])
+def delete(index):
+   if 0 <= index < len(tarefas):  # Verifica se o índice está dentro do intervalo
+        tarefas.pop(index)          # Remove a tarefa do índice especificado
+   return redirect(url_for('index'))
+if __name__ == "__main__":
+    app.run(debug=True)
